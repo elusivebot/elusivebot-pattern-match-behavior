22d8dc046226978ed84f70cc10dcd3006100de08
Bryan DeGrendel <bryan@degrendel.com>
2024-08-10 19:01:35 -0400

    Rework to use common Kotlin interface for Kafka

--- app/src/main/kotlin/com/sirnuke/elusivebot/behaviors/pattern/match/app/App.kt
+++ app/src/main/kotlin/com/sirnuke/elusivebot/behaviors/pattern/match/app/App.kt
@@ -5,77 +5,48 @@
 package com.sirnuke.elusivebot.behaviors.pattern.match.app
 
 import com.sirnuke.elusivebot.behaviors.pattern.match.lib.PatternMatchService
+import com.sirnuke.elusivebot.common.Kafka
 import com.sirnuke.elusivebot.schema.ChatMessage
 import com.uchuhimo.konf.Config
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.StreamsConfig
-import org.apache.kafka.streams.kstream.KStream
 import org.slf4j.LoggerFactory
 
 import java.util.concurrent.atomic.AtomicBoolean
 
 import kotlin.concurrent.thread
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @Suppress("TOO_LONG_FUNCTION")
-fun main() {
+fun main() = runBlocking {
     val log = LoggerFactory.getLogger("com.sirnuke.elusivebot.behaviors.pattern.match.AppKt")!!
-    log.info("Starting Pattern Match service")
     val config = Config { addSpec(PatternMatchSpec) }.from.env()
+    val running = AtomicBoolean(true)
 
     log.info("Starting Pattern Match; producer {} & consumer {}", config[PatternMatchSpec.Kafka.producerTopic],
         config[PatternMatchSpec.Kafka.consumerTopic])
 
     val service = PatternMatchService.create()
 
-    val running = AtomicBoolean(true)
-
-    val consumerConfig = StreamsConfig(
-        mapOf<String, Any>(
-            StreamsConfig.APPLICATION_ID_CONFIG to config[PatternMatchSpec.serviceId],
-            StreamsConfig.BOOTSTRAP_SERVERS_CONFIG to config[PatternMatchSpec.Kafka.bootstrap],
-            StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG to Serdes.String().javaClass.name,
-            StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG to Serdes.String().javaClass.name
-        )
-    )
-
-    val builder = StreamsBuilder()
-
-    val producerConfig = mapOf(
-        "bootstrap.servers" to config[PatternMatchSpec.Kafka.bootstrap],
-        "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
-        "value.serializer" to "org.apache.kafka.common.serialization.StringSerializer"
-    )
-
-    val producer: KafkaProducer<String, String> = KafkaProducer(producerConfig)
-
-    val consumer: KStream<String, String> = builder.stream(config[PatternMatchSpec.Kafka.consumerTopic])
-
-    consumer.foreach { key, raw ->
-        log.info("Got input {} {}", key, raw)
-        val message: ChatMessage = Json.decodeFromString(raw)
-        service.process(message.message)?.let {
-            log.info("Sending {} -> {}", message.message, it)
-            val response = ChatMessage(header = message.header, user = null, message = it)
-            producer.send(
-                ProducerRecord(
-                    config[PatternMatchSpec.Kafka.producerTopic], key, Json.encodeToString(response)
-                )
-            )
+    val kafka = Kafka.Builder(
+        applicationId = config[PatternMatchSpec.serviceId],
+        bootstrap = config[PatternMatchSpec.Kafka.bootstrap],
+        scope = this,
+    ).registerConsumer(config[PatternMatchSpec.Kafka.consumerTopic]) { producer, key, msg: ChatMessage ->
+        log.info("Got input {} {}", key, msg)
+        service.process(msg.message)?.let { answer ->
+            log.info("Sending {} -> {}", msg.message, answer)
+            val response = ChatMessage(header = msg.header, user = null, message = answer)
+            producer.send(config[PatternMatchSpec.Kafka.producerTopic], key, Json.encodeToString(response)) { _, ex ->
+                ex?.let {
+                    log.error("Unable to send response to {}", key, it)
+                } ?: log.info("Done sending response on {}", key)
+            }
         }
-    }
-
-    val streams = KafkaStreams(builder.build(), consumerConfig)
-    streams.start()
+    }.build()
 
     Runtime.getRuntime().addShutdownHook(thread(start = false, name = "shutdown-hook") {
         running.set(false)
-        streams.close()
-        producer.close()
+        kafka.close()
     })
 }
